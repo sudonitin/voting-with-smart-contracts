12da2ff5cf4fdc8e47bdcef400fa6959ee8a60ec
globefire <sahun5774@gmail.com>
2018-12-21 11:12:25 +0530

    app.js modified

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,20 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
 
   init: function() {
-    return await App.initWeb3();
+    return App.initWeb3();
   },
 
   initWeb3: function() {
+    
     if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
+      // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
@@ -18,42 +22,48 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON("Electionjson", function(election) {
+    $.getJSON("Election.json", function(election) {
+      // create a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
+      // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
-    
+
       return App.render();
-    });    
+    });
   },
 
   render: function() {
-    var electionInstace;
+    var electionInstance;
     var loader = $("#loader");
-    var content = $("content");
+    var content = $("#content");
 
     loader.show();
     content.hide();
 
+    // Load account data
     web3.eth.getCoinbase(function(err, account) {
-      if (err == null) {
+      if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
       }
     });
 
+    // Load contract data
     App.contracts.Election.deployed().then(function(instance) {
-      electionInstace = instance;
-      return electionInstace.candidatesCount();
+      electionInstance = instance;
+      return electionInstance.candidatesCount();
     }).then(function(candidatesCount) {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
-      for (var i = 0; i < candidatesCount; i++) {
-        electionInstace.candidates(i).then(function(candidate) {
+      for (var i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
           var id = candidate[0];
           var name = candidate[1];
           var voteCount = candidate[2];
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name "</td><td>" + voteCount + "</td></tr>"
+
+          // submit candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
         });
       }
